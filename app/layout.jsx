6ba95734f5537a5f74fb2bf5c76bff6a22f86a0a
dayo-odesola <dayoodesola2001@gmail.com>
2024-02-27 15:18:25 +0100

    added navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "PropertyPulse | Find The Perfect Rental",
@@ -8,7 +9,8 @@ export const metadata = {
 const MainLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <Navbar />
+      <main>{children}</main>
     </html>
   );
 };
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">Welcome</h1>
+      <Link href="/properties">Show properties</Link>
+    </div>
+  );
 };
 
 export default Homepage;
