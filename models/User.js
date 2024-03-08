8607e68ca9bea3c796715aa30c52a41ac0f67cc6
dayo-odesola <dayoodesola2001@gmail.com>
2024-03-08 13:36:33 +0100

    property and user model

---
+++ models/User.js
@@ -0,0 +1,29 @@
+import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: string,
+      unique: [true, "Email already exists"],
+      required: [true, "Email is required"],
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+    },
+    image: {
+      type: String,
+    },
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Property", // the collection we are getting the id from
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const User = userSchema.Model("User", userSchema);
+
+export default User;
